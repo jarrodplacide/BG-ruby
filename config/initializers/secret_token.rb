3456562b28cf2fc6a340990ff038e637cc82abbe
Jarrod Placide-Raymond <royaltyclubvp@royalty-club.com>
2013-10-16 02:30:51 -0400

    Initial Commit of Rails Environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Buzzygals::Application.config.secret_key_base = '6b99fcba009a663f1417cc3ec225e5b31eef279645cc3fa082ff20ec2e8a9f5aaaa3788b503bd70fada504dfd61e5a83c1e92e6c5dce8208deb16d007e2f6523'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Buzzygals::Application.config.session_store :cookie_store, key: '_buzzygals_session'
